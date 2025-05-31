68a3fe6393985f1dcd24c4cc92fd4c3f9c3688b4
ivanVgusev <165506336+ivanVgusev@users.noreply.github.com>
2025-05-31 03:47:38 +0300

    Massive literature parser update; Normalisation, possibility to process multiple paths with one call

--- lit_parser.py
+++ lit_parser.py
@@ -1,18 +1,117 @@
 import os
 from n_grams import n_grams_main
-from readers import fb2reader, txt_reader
+from readers import fb2reader, txt_linesreader
 import time
 from statistical_methods import jaccard, tanimoto
 from progress_monitor import progress_bar
+import math
+import pymorphy2
+import nltk
+from string_cleaner import complex_cleaner
+from pathlib import Path
+import itertools
 
 
 def txt_writer(data, filepath, encoding='utf-8'):
-    if type(data) is str:
-        pass
-    else:
+    if type(data) is not str:
         data = str(data)
+
+    if not filepath.endswith('.txt'):
+        filepath += '.txt'
+
     with open(filepath, 'w', encoding=encoding) as f:
         f.write(data)
+
+
+def text_normaliser(m_path, lit_folder_name):
+    inp_path = str(os.path.join(m_path, lit_folder_name))
+    normalised_literature_folder = str(os.path.join(m_path, f'{lit_folder_name}_normalised'))
+    os.makedirs(os.path.join(normalised_literature_folder), exist_ok=True)
+
+    already_normalised = []
+    yet_to_normalise = 0
+    for root, _, files in os.walk(inp_path):
+        for file in files:
+            if file.startswith('.'):
+                continue
+            path = Path(str(os.path.join(root, file)))
+            path_parts = list(path.parts)
+            lit_folder_index = path_parts.index(lit_folder_name)
+
+            pre_index = path_parts[:lit_folder_index]
+            post_index = path_parts[lit_folder_index + 1:]
+            normalised_path = os.path.join(*pre_index, f'{lit_folder_name}_normalised', *post_index)
+            if not normalised_path.endswith('.txt'):
+                normalised_path = os.path.splitext(normalised_path)[0] + '.txt'
+
+            if not os.path.exists(normalised_path):
+                yet_to_normalise += 1
+            else:
+                basename = os.path.basename(normalised_path)[1]
+                already_normalised.append(basename)
+
+    if yet_to_normalise == 0:
+        return print("[text_normaliser] /// All the texts have already been normalised, so we're good to go!")
+    else:
+        print(f'[text_normaliser] /// Looks like we need to normalise {yet_to_normalise} text(s)')
+        iteration_counter = 0
+
+        for root, _, files in os.walk(inp_path):
+            for file in files:
+                if file.startswith('.'):
+                    continue
+                if file in already_normalised:
+                    continue
+                # progress bar updating
+                iteration_counter += 1
+                progress_bar(yet_to_normalise, iteration_counter)
+
+                full_non_normalised_path = os.path.join(root, file)
+                path = Path(full_non_normalised_path)
+                path_parts = list(path.parts)
+                lit_folder_index = path_parts.index(lit_folder_name)
+
+                pre_index = path_parts[:lit_folder_index]
+                post_index = path_parts[lit_folder_index + 1:]
+
+                full_normalised_path = os.path.join(*pre_index, f'{lit_folder_name}_normalised', *post_index)
+                full_normalised_path = os.path.splitext(full_normalised_path)[0]
+                path = Path(full_normalised_path)
+                path_parts = list(path.parts)
+                full_normalised_path_no_basename = path_parts[:-1]
+                full_normalised_path_no_basename = os.path.join(*full_normalised_path_no_basename)
+
+                if file.endswith('.fb2'):
+                    try:
+                        text = fb2reader(full_non_normalised_path)
+                    except Exception as e:
+                        # print(f'An error {e} occurred!')
+                        continue
+                elif file.endswith('.txt'):
+                    try:
+                        text = txt_linesreader(full_non_normalised_path)
+                    except Exception as e:
+                        # print(f'An error {e} occurred!')
+                        continue
+                else:
+                    continue
+
+                normalised_text = ''
+                for lines in text:
+                    sentences = nltk.tokenize.sent_tokenize(lines, "russian")
+                    for sentence in sentences:
+                        sentence = str(complex_cleaner(sentence))
+                        sentence = sentence.lower()
+                        sentence = nltk.tokenize.word_tokenize(sentence, "russian")
+
+                        if len(sentence) == 0:
+                            continue
+                        normalised_sentence = [morph.parse(i)[0].normal_form for i in sentence]
+                        normalised_text += ' '.join(normalised_sentence) + ' '
+
+                os.makedirs(full_normalised_path_no_basename, exist_ok=True)
+
+                txt_writer(normalised_text, full_normalised_path)
 
 
 def text_fetcher(repo_path: str) -> dict:
@@ -22,17 +121,18 @@ def text_fetcher(repo_path: str) -> dict:
             if file.startswith('.'):
                 continue
 
+            full_path = os.path.join(root, file)
             if file.endswith('.fb2'):
                 try:
-                    text = fb2reader(root + '/' + file)
+                    text = fb2reader(full_path)
                     litcorpus.update({file: text})
                 except Exception as e:
                     # print(f'An error {e} occurred!')
                     continue
 
-            if file.endswith('.txt'):
+            elif file.endswith('.txt'):
                 try:
-                    text = txt_reader(root + '/' + file)
+                    text = txt_linesreader(full_path)
                     litcorpus.update({file: text})
                 except Exception as e:
                     # print(f'An error {e} occurred!')
@@ -43,16 +143,28 @@ def text_fetcher(repo_path: str) -> dict:
 
 # Jaccard ngrams, Jaccard vocabulary and Tanimoto ngrams counters
 def similarity_measurer(litcorpus1: dict, litcorpus2: dict, N) -> dict:
-    total_iterations = len(litcorpus1) * len(litcorpus2)
-    iteration_counter = 0
+    k = 2
+    all_combinations = list(itertools.product(litcorpus1, litcorpus2))
+    unique_combinations = len([(a, b) for a, b in all_combinations if a != b])
 
+    if litcorpus1 == litcorpus2:
+        unique_combinations_condensed = int(unique_combinations / k)
+    else:
+        unique_combinations_condensed = int(unique_combinations)
+
+    iteration_counter = 0
     stats_dict = {}
+    analysed_books_pairs = []
     for text1 in litcorpus1:
-        loop_start = time.time()
         for text2 in litcorpus2:
+            if text1 == text2:
+                continue
+            if sorted([text1, text2]) in analysed_books_pairs:
+                continue
+
             # progress bar updating
             iteration_counter += 1
-            progress_bar(total_iterations, iteration_counter)
+            progress_bar(unique_combinations_condensed, iteration_counter)
 
             ngram_counter_1, vocab_1, n_gram_1 = n_grams_main(litcorpus1.get(text1), N)
             ngram_counter_2, vocab_2, n_gram_2 = n_grams_main(litcorpus2.get(text2), N)
@@ -64,33 +176,131 @@ def similarity_measurer(litcorpus1: dict, litcorpus2: dict, N) -> dict:
             # tanimoto ngram_counter similarity measures weighted vectors i.e. ngram counters
             tanimoto_ngram_counter_similarity = tanimoto(ngram_counter_1, ngram_counter_2)
 
-            # excluding text == text situations
-            if jaccard_ngram_similarity and jaccard_vocab_similarity and tanimoto_ngram_counter_similarity == 1.0:
-                continue
-
             texts_similarity = {f'{text1} – {text2}': {'jaccard_ngram': jaccard_ngram_similarity,
                                                        'jaccard_vocab': jaccard_vocab_similarity,
                                                        'tanimoto_ngram_counter': tanimoto_ngram_counter_similarity}}
             stats_dict.update(texts_similarity)
-        loop_end = time.time()
-        print(f' /// This iteration took {loop_end - loop_start} seconds.')
+            analysed_books_pairs.append(sorted([text1, text2]))
     return stats_dict
 
 
-path1 = '/Users/ivanguseff/PycharmProjects/LitSim/literature/Толстой'
-# path2 = '/Users/ivanguseff/PycharmProjects/LitSim/literature/Тургенев'
-path2 = path1
+def auth1_auth1(author_folder_filepath, N):
+    corpus1 = text_fetcher(author_folder_filepath)
+    corpus2 = corpus1
+    stats = similarity_measurer(corpus1, corpus2, N)
+    return stats
 
-name = 'tolstoy–tolstoy.txt'
-output_path = '/Users/ivanguseff/PycharmProjects/LitSim/results/N=2/auth1–auth2'
 
+def auth1_auth2(author_folder_filepath1, author_folder_filepath2, N):
+    corpus1 = text_fetcher(author_folder_filepath1)
+    corpus2 = text_fetcher(author_folder_filepath2)
+    stats = similarity_measurer(corpus1, corpus2, N)
+    return stats
+
+
+def wholesale_processing_auth1_auth1(m_path, lit_folder_name, N, normalised=True):
+    inp_path = str(os.path.join(m_path, lit_folder_name))
+    if normalised:
+        text_normaliser(m_path, lit_folder_name)
+        inp_path = str(os.path.join(m_path, f'{lit_folder_name}_normalised'))
+    author_directories = os.listdir(inp_path)
+    n = len(author_directories)
+    if '.DS_Store' in author_directories:
+        n -= 1
+    print(f'[wholesale_processing_auth1_auth1] /// '
+          f'There are {n} author directories => {n} possible non-repeated combination(s)')
+
+    for folder in author_directories:
+        if folder.startswith('.'):
+            continue
+
+        folder_full_path = os.path.join(inp_path, folder)
+        basename = os.path.basename(folder_full_path)
+
+        if normalised:
+            output_path = os.path.join(m_path, 'results_normalised', f'N={N}', 'auth1–auth1')
+        else:
+            output_path = os.path.join(m_path, 'results', f'N={N}', 'auth1–auth1')
+
+        os.makedirs(output_path, exist_ok=True)
+
+        output_path = os.path.join(output_path, f'{basename}–{basename}.txt')
+
+        stats = auth1_auth1(folder_full_path, N)
+
+        txt_writer(stats, output_path)
+
+
+def wholesale_processing_auth1_auth2(m_path, lit_folder_name, N, normalised=True):
+    inp_path = str(os.path.join(m_path, lit_folder_name))
+
+    if normalised:
+        text_normaliser(m_path, lit_folder_name)
+        inp_path = str(os.path.join(m_path, f'{lit_folder_name}_normalised'))
+
+    author_directories = os.listdir(inp_path)
+    n = len(author_directories)
+    if '.DS_Store' in author_directories:
+        n -= 1
+    k = 2
+    combinations_amount = int(math.factorial(n) / (math.factorial(n - k) * math.factorial(k)))
+    print(f'[wholesale_processing_auth1_auth2] /// '
+          f'There are {n} author directories => {combinations_amount} possible non-repeated combination(s)')
+
+    analysed_author_pairs = []
+    for folder1 in author_directories:
+        if folder1.startswith('.'):
+            continue
+        for folder2 in author_directories:
+            if folder2.startswith('.'):
+                continue
+            if folder1 == folder2:
+                continue
+            # checking the auth1–auth2, auth2–auth1 situations
+            # i.e. when the combination appears two times but with different element placements
+            if sorted([folder1, folder2]) in analysed_author_pairs:
+                continue
+
+            folder1_full_path = os.path.join(inp_path, folder1)
+            folder2_full_path = os.path.join(inp_path, folder2)
+            basename1 = os.path.basename(folder1_full_path)
+            basename2 = os.path.basename(folder2_full_path)
+
+            if normalised:
+                output_path = os.path.join(m_path, 'results_normalised', f'N={N}', 'auth1–auth2')
+            else:
+                output_path = os.path.join(m_path, 'results', f'N={N}', 'auth1–auth2')
+
+            os.makedirs(output_path, exist_ok=True)
+
+            output_path = os.path.join(output_path, f'{basename1}–{basename2}.txt')
+
+            stats = auth1_auth2(folder1_full_path, folder2_full_path, N)
+
+            txt_writer(stats, output_path)
+
+            analysed_author_pairs.append(sorted([folder1, folder2]))
+
+
+morph = pymorphy2.MorphAnalyzer()
+try:
+    nltk.data.find('tokenizers/punkt_tab')
+except LookupError:
+    nltk.download('punkt_tab')
+
+main_path = os.path.join("/Users", "ivanguseff", "PycharmProjects", "LitSim")
+literature_folder_name = 'literature'
+
+# main reacher
 start_time = time.time()
-
-corpus1 = text_fetcher(path1)
-corpus2 = text_fetcher(path2)
-stats = similarity_measurer(corpus1, corpus2, 2)
-
-txt_writer(stats, output_path + '/' + name)
-
+n_grams_confing = [2, 3, 4]
+normalisation_confing = [True, False]
+config_combinations = itertools.product(n_grams_confing, normalisation_confing)
+for congfig in config_combinations:
+    N_config = congfig[0]
+    normalisation_config = congfig[1]
+    wholesale_processing_auth1_auth2(main_path, literature_folder_name, N_config, normalisation_config)
+    wholesale_processing_auth1_auth1(main_path, literature_folder_name, N_config, normalisation_config)
 end_time = time.time()
+
 print(f'Achieved in: {end_time - start_time}')

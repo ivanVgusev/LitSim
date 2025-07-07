2b02769210c8c80651c18e70d3173504a03c78a3
ivanVgusev <165506336+ivanVgusev@users.noreply.github.com>
2025-07-07 17:48:54 +0300

    Updated machine learning, added comments

--- ml.py
+++ ml.py
@@ -1,79 +1,111 @@
-from sklearn.neighbors import KNeighborsClassifier  # К-ближайших соседей
-from sklearn.tree import DecisionTreeClassifier  # дерево принятия решений
-from sklearn.svm import SVC  # метод опорных векторов
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix  # оценка
-import matplotlib.pyplot as plt
-import numpy as np
-
-from readers import txt_linesreader
 import os
 import statistics
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
 
-"""
-0 - same author
-1 - different author
-"""
+from readers import txt_linesreader
 
-clf = DecisionTreeClassifier()
+# Labeling:
+# 0 - same author
+# 1 - different author
+
+# Default classifier
+default_clf = DecisionTreeClassifier()
 
 
-def training_test(author1_author1, author1_author2):
-    data_x = author1_author1 + author1_author2
-    data_y = [0 for _ in range(len(author1_author1))] + [1 for _ in range(len(author1_author2))]
-    x_train, x_test, y_train, y_test = train_test_split(data_x, data_y, test_size=0.2)
+def compute_metrics(author1_data: list, author2_data: list, clf=default_clf):
+    """
+    Train a classifier and compute evaluation metrics.
+
+    :param author1_data: Feature vectors for same author (label 0)
+    :param author2_data: Feature vectors for different author (label 1)
+    :param clf: Classifier object (default: DecisionTreeClassifier)
+    :return: Tuple of (accuracy, precision, recall, confusion_matrix)
+    """
+    data_x = author1_data + author2_data
+    data_y = [0] * len(author1_data) + [1] * len(author2_data)
+
+    x_train, x_test, y_train, y_test = train_test_split(data_x, data_y, test_size=0.2, random_state=42)
 
     clf.fit(x_train, y_train)
-    y_prediction = clf.predict(x_test)
+    y_pred = clf.predict(x_test)
 
-    accuracy_s = accuracy_score(y_test, y_prediction)
-    precision_s = precision_score(y_test, y_prediction)
-    recall_s = recall_score(y_test, y_prediction)
-    conf_matrix_s = confusion_matrix(y_test, y_prediction)
+    return (
+        accuracy_score(y_test, y_pred),
+        precision_score(y_test, y_pred),
+        recall_score(y_test, y_pred),
+        confusion_matrix(y_test, y_pred)
+    )
 
-    return accuracy_s, precision_s, recall_s, conf_matrix_s
 
+def predict(author1_data: list, author2_data: list, x_test: list, clf=default_clf):
+    """
+    Train classifier on training data and predict labels for new samples.
 
-def training(author1_author1, author1_author2, x_test):
-    x_train = author1_author1 + author1_author2
-    y_train = [0 for _ in range(len(author1_author1))] + [1 for _ in range(len(author1_author2))]
+    :param author1_data: Samples for label 0
+    :param author2_data: Samples for label 1
+    :param x_test: Samples to predict
+    :param clf: Classifier object
+    :return: List of predicted labels
+    """
+    x_train = author1_data + author2_data
+    y_train = [0] * len(author1_data) + [1] * len(author2_data)
 
     clf.fit(x_train, y_train)
-    y_prediction = clf.predict(x_test)
-
-    return y_prediction
+    return clf.predict(x_test)
 
 
-def wholesale_parameters_measurer(author1_author1, author1_author2):
-    path = '/Users/ivanguseff/PycharmProjects/LitSim/results_normalised/N=4/'
-    auth1_auth1 = []
-    auth1_auth2 = []
+def load_feature_vectors(folder_path: str):
+    """
+    Load and parse feature vectors from a directory structure.
 
-    for root, _, files in os.walk(path):
+    :param folder_path: Root directory containing 'auth1–auth1' and 'auth1–auth2' subfolders.
+    :return: Tuple of (auth1_auth1_data, auth1_auth2_data)
+    """
+    aurtor1_author1_data = []
+    author1_author2_data = []
+
+    for root, _, files in os.walk(folder_path):
         for file in files:
             if file.startswith('.'):
-                continue
-            stats = txt_linesreader(root + '/' + file)[0]
-            stats = eval(stats)
+                continue  # Skip hidden/system files
+            stats_raw = txt_linesreader(str(os.path.join(root, file)))[0]
+            stats = eval(stats_raw)
+
+            values = [list(sample.values()) for sample in stats.values()]
             if 'auth1–auth1' in root:
-                book_values = list(stats.values())
-                book_values = [list(i.values()) for i in book_values]
-                auth1_auth1.extend(book_values)
+                aurtor1_author1_data.extend(values)
             elif 'auth1–auth2' in root:
-                book_values = list(stats.values())
-                book_values = [list(i.values()) for i in book_values]
-                auth1_auth2.extend(book_values)
+                author1_author2_data.extend(values)
 
-    accuracy = []
-    precision = []
-    recall = []
-    conf_matrix = []
-    for i in range(100):
-        accuracy.append(training_test(author1_author1, author1_author2)[0])
-        precision.append(training_test(author1_author1, author1_author2)[1])
-        recall.append(training_test(author1_author1, author1_author2)[2])
-        conf_matrix.append(training_test(author1_author1, author1_author2)[3])
+    return aurtor1_author1_data, author1_author2_data
 
-    print('Accuracy:', statistics.mean(accuracy))
-    print('Precision:', statistics.mean(precision))
-    print('Recall:', statistics.mean(recall))
+
+def measure_model_performance(author1_author1_data, author1_author2_data, n_trials=100, clf=default_clf):
+    """
+    Measure model performance over multiple trials and compute average metrics.
+
+    :param author1_author1_data: list of lists containing info from the same authors
+    :param author1_author2_data: list of lists containing info from different authors
+    :param author1_author1_data: Samples for label 0
+    :param author1_author2_data: Samples for label 1
+    :param n_trials: Number of times to repeat training/testing
+    :param clf: Classifier object
+    :return: Dictionary with mean accuracy, precision, and recall
+    """
+    accuracy_scores = []
+    precision_scores = []
+    recall_scores = []
+
+    for _ in range(n_trials):
+        acc, prec, rec, _ = compute_metrics(author1_author1_data, author1_author2_data, clf)
+        accuracy_scores.append(acc)
+        precision_scores.append(prec)
+        recall_scores.append(rec)
+
+    return {
+        'accuracy': statistics.mean(accuracy_scores),
+        'precision': statistics.mean(precision_scores),
+        'recall': statistics.mean(recall_scores)
+    }

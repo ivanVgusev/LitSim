6cb32282a9d64b2502f6907b956cdf7dc02f3985
ivanVgusev <165506336+ivanVgusev@users.noreply.github.com>
2025-07-08 23:57:31 +0300

    Minor improvements

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from n_grams import n_grams_main
 from statistical_methods import jaccard, tanimoto
 
 
-def extract_features(text1, text2, n=3, normalize=False):
-    if normalize:
+def extract_features(text1, text2, n=3, normalise=False):
+    if normalise:
         text1 = text_normaliser(text1)
         text2 = text_normaliser(text2)
 
@@ -44,7 +44,7 @@ def compare_authors(file1_path, file2_path, stats_path, n=3, normalise=False):
     book1 = txt_linesreader(file1_path)
     book2 = txt_linesreader(file2_path)
 
-    test_features = [extract_features(book1, book2, n=n, normalize=normalise)]
+    test_features = [extract_features(book1, book2, n=n, normalise=normalise)]
 
     if normalise is True:
         stats_path += 'results_normalised/'
@@ -62,7 +62,8 @@ if __name__ == "__main__":
     test_folder = '/Users/ivanguseff/PycharmProjects/LitSim/literature_test/'
     train_folder = '/Users/ivanguseff/PycharmProjects/LitSim/'
 
-    file1 = os.path.join(test_folder, 'Булгаков Михаил. Собачье сердце.txt')
-    file2 = os.path.join(test_folder, 'Шукшин Василий. Калина красная.txt')
+    file1 = os.path.join(test_folder, 'Горький_Мать.txt')
+    # file2 = os.path.join(test_folder, 'Горький_Детство_В_людях_Мои_университеты.txt')
+    file2 = os.path.join(test_folder, 'Достоевский_Подросток.txt')
 
     compare_authors(file1, file2, train_folder, n=3, normalise=False)

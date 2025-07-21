e2594cfac1e37765d8ddbbec9524d528d5495e5c
ivanVgusev <165506336+ivanVgusev@users.noreply.github.com>
2025-07-22 02:07:51 +0300

    upd: writers added & fb2 and epub readers were switched to read instead of linesread mode

--- writers_and_readers.py
+++ writers_and_readers.py
@@ -22,17 +22,26 @@ def detect_encoding(file_path: str):
     return encoding
 
 
-def fb2reader(fb2_filepath: str):
+"""
+Code section with readers.
+    Included:
+        .fb2
+        .txt
+        .epub
+"""
+
+
+def fb2reader(fb2_filepath: str) -> str:
     tree = etree.parse(fb2_filepath)
     t_root = tree.getroot()
 
     bodies = t_root.xpath('//fictionbook:body', namespaces=NSMAP)
 
-    book = []
+    book = ''
     for body in bodies:
         paragraphs = body.xpath('.//fictionbook:p/text()', namespaces=NSMAP)
         for p in paragraphs:
-            book.append(p.strip())
+            book += (p.strip()) + ' '
     return book
 
 
@@ -48,19 +57,38 @@ def txt_reader(txt_filepath: str) -> str:
         return f.read()
 
 
-def epub_reader(epub_filepath: str):
+def epub_reader(epub_filepath: str) -> str:
     try:
         encoding = detect_encoding(epub_filepath)
 
         book = epub.read_epub(epub_filepath)
         chapters = book.get_items_of_type(ebooklib.ITEM_DOCUMENT)
-        full_text = []
+        full_text = ''
 
         for chapter in chapters:
             content = chapter.get_content().decode(encoding)
             soup = BeautifulSoup(content, 'html.parser')
             chapter_text = soup.get_text().strip()
-            full_text.append(chapter_text)
+            full_text += chapter_text + ' '
+
         return full_text
     except epub.EpubException:
         return '0'
+
+
+"""
+Code section with writers.
+    Included:
+        .txt
+"""
+
+
+def txt_writer(data, filepath, encoding='utf-8'):
+    if type(data) is not str:
+        data = str(data)
+
+    if not filepath.endswith('.txt'):
+        filepath += '.txt'
+
+    with open(filepath, 'w', encoding=encoding) as f:
+        f.write(data)
